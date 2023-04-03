9ebdcb49b348aa18c36325d563b8fc6cd0e32938
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-04 00:04:54 +0600

    practice c

--- practice.c
+++ practice.c
@@ -4,7 +4,9 @@ int main()
     printf("Recently I heard that you've achieved 95%% marks in your exam.\n");
     printf("This is brilliant!\n"); 
     printf("I wish you'll shine in your life!\tGood luck with all the barriers(/\\) in\nyour life.\n");
-    printf("\"Assalamu Alaikum\"");
+    printf("\"Assalamu Alaikum\"\n");
+
+    printf("Alhamdulillah for everything\n");
 
     return 0;
 }
